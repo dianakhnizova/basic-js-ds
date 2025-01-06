6cd88b2b89af29711aa090eca1406d82b83af3ef
dianakhnizova <khnizovad@gmail.com>
2025-01-07 00:16:24 +0600

    fix: fix task - queue

--- src/queue.js
+++ src/queue.js
@@ -14,33 +14,30 @@ const { ListNode } = require('../extensions/list-node.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
   getUnderlyingList() {
     return this.head;
   }
 
   enqueue(value) {
     const newNode = new ListNode(value);
-    
     if (!this.tail) {
-      this.head = newNode;  
-      this.tail = newNode;
+      this.head = this.tail = newNode;
     } else {
-      this.tail.next = newNode;  
-      this.tail = newNode;  
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
   }
 
   dequeue() {
-    if (!this.head) {
-      return null;  
-    }
-    const dequeuedValue = this.head.value;  
-    this.head = this.head.next;  
-    if (!this.head) {
-      this.tail = null;  
-    }
-    return dequeuedValue;
+    if (!this.head) return null;
+    const value = this.head.value;
+    this.head = this.head.next;
+    if (!this.head) this.tail = null; 
+    return value;
   }
 }
 
